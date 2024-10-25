b4f28727d10ed27c84f864be1f2f9bab17a41954
superwx1985 <superwx1985@gmail.com>
2024-10-25 12:44:02 +0800

    change to register endpoint and logic

--- R3_registrar.py
+++ R3_registrar.py
@@ -93,7 +93,7 @@ def register(env, device_type, row, sn, mcu1, mcu2, whole_device=True, logger=ge
             if register_mower_app_board_rep.status_code == 200 and row in _["data"]["success"]:
                 logger.info(f"{row} mower_app_board register successfully")
             else:
-                logger.error(f"mower_app_board register failed: {_["message"]}")
+                logger.error(f"mower_app_board register failed: {_}")
 
             if whole_device:
                 register_mower_info_payload = f"{row};000028;{formatted_time};{sn};1109-030-B-10A:010;1109-030-B-10B:3;1109-030-B-10C:01;1109-030-B-10D:000000000;1109-030-B-10E:00;1109-030-B-10F:0000000000;1109-030-B-20A:040077800;1109-030-B-20B:06;1109-030-B-20C:235040013;1109-030-B-20D:1703887890;1109-030-B-25A:040085400;1109-030-B-25B:05;1109-030-B-25C:1704192278;1109-030-B-30:000000000021;1109-030-B-40:001693398400;1109-030-B-45:001703254987;1109-030-B-50A:000000000000000;1109-030-B-50B:00000000000000000000;1109-030-B-50C:000000000000000000000000000000;1109-030-B-60A:000000000;1109-030-B-60B:00;1109-030-B-60C:0000000000;1109-030-B-60D:00000000;1109-030-B-60E:000000000;1109-030-B-60F:000;1109-030-B-60G:0;1109-030-B-60H:00;1109-030-B-60I:000;1109-030-B-60J:0;1109-030-B-60K:00;1109-030-B-60L:000000000;1109-030-B-60M:00;1109-030-B-60N:0;1109-030-B-70:0000;"
@@ -102,9 +102,9 @@ def register(env, device_type, row, sn, mcu1, mcu2, whole_device=True, logger=ge
                 if register_mower_info_rep.status_code == 200 and row in _["data"]["success"]:
                     logger.info(f"{row} mower_info register successfully")
                 else:
-                    logger.error(f"mower_info register failed: {_["message"]}")
+                    logger.error(f"mower_info register failed: {_}")
         else:
-            register_ra_info_payload = f"{row};000038;{formatted_time};{sn};{mcu1};{mcu2};16;1;1;040099000;04;"
+            register_ra_info_payload = f"{row};000038;{formatted_time};{sn};{mcu1};{mcu2};222;1;0;040099000;05;"
             register_ra_info_rep = request("POST", f"{centralized_endpoint_api}/v3/service/common/RA/device/3",
                                                    params={"fileName": "3.csv"}, headers={"Access-Token": access_token},
                                                    data=register_ra_info_payload, logger=logger,
@@ -113,7 +113,7 @@ def register(env, device_type, row, sn, mcu1, mcu2, whole_device=True, logger=ge
             if register_ra_info_rep.status_code == 200 and row in _["data"]["success"]:
                 logger.info(f"{row} ra_info register successfully")
             else:
-                logger.error(f"ra_info register failed: {_["message"]}")
+                logger.error(f"ra_info register failed: {_}")
 
     except Exception as e:
         logger.error(e)
